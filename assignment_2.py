6d1bf2b664ae4be4134506e3446a97b83ecc6c37
Avraj Singh <ASingh@spartaglobal.com>
2020-02-05 23:29:32 +0000

    added section 1

---
+++ assignment_2.py
@@ -0,0 +1,8 @@
+# section 1
+name_dict = {
+    'Beginning': 'In the beginning there was our god',
+    'Middle': 'Then came the universe with all its glory',
+    'End': 'After millions of years of peace there was war, that threaten the universe.'
+}
+
+#
